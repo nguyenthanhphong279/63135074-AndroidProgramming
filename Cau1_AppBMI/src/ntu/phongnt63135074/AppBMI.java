18467b8c390005a13dc4a3b89f820fc964414d5e
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-24 20:02:39 +0700

    Cập nhật giao diện appBMI

--- Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
+++ Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
@@ -10,6 +10,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class AppBMI extends JFrame {
 
@@ -28,20 +29,20 @@ public class AppBMI extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Phần Mềm Tính Chỉ Số BMI");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
-		lblNewLabel.setBounds(118, 10, 339, 44);
-		contentPane.add(lblNewLabel);
+		JLabel lblPhammem = new JLabel("Phần Mềm Tính Chỉ Số BMI");
+		lblPhammem.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		lblPhammem.setBounds(118, 10, 339, 44);
+		contentPane.add(lblPhammem);
 		
-		JLabel lblNewLabel_1 = new JLabel("Chiều cao (cm):");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(10, 77, 177, 34);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblChieuCao = new JLabel("Chiều cao (cm):");
+		lblChieuCao.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblChieuCao.setBounds(10, 77, 177, 34);
+		contentPane.add(lblChieuCao);
 		
-		JLabel lblNewLabel_2 = new JLabel("Cân Nặng (kg):");
-		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		lblNewLabel_2.setBounds(10, 134, 165, 44);
-		contentPane.add(lblNewLabel_2);
+		JLabel lblCanNang = new JLabel("Cân Nặng (kg):");
+		lblCanNang.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblCanNang.setBounds(10, 134, 165, 44);
+		contentPane.add(lblCanNang);
 		
 		textField = new JTextField();
 		textField.setBounds(161, 80, 312, 34);
@@ -54,7 +55,45 @@ public class AppBMI extends JFrame {
 		textField_1.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Tính");
-		btnNewButton.setBounds(263, 216, 85, 21);
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		btnNewButton.setBackground(new Color(255, 0, 0));
+		btnNewButton.setBounds(234, 209, 114, 34);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel_3 = new JLabel("Thiếu cân <18.5");
+		lblNewLabel_3.setBackground(new Color(128, 255, 255));
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(10, 357, 133, 44);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Cân đối 18.5-24.5");
+		lblNewLabel_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3_1.setBackground(new Color(0, 255, 0));
+		lblNewLabel_3_1.setBounds(199, 357, 151, 44);
+		contentPane.add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_2 = new JLabel("Thừa cân 25-29.9");
+		lblNewLabel_3_2.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3_2.setBackground(new Color(255, 255, 0));
+		lblNewLabel_3_2.setBounds(416, 357, 144, 44);
+		contentPane.add(lblNewLabel_3_2);
+		
+		JLabel lblNewLabel_3_3 = new JLabel("Béo phì 30-34.9");
+		lblNewLabel_3_3.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3_3.setBackground(new Color(255, 128, 64));
+		lblNewLabel_3_3.setBounds(118, 411, 133, 44);
+		contentPane.add(lblNewLabel_3_3);
+		
+		JLabel lblNewLabel_3_4 = new JLabel("Béo phì nguy hiểm >34.9");
+		lblNewLabel_3_4.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3_4.setBackground(new Color(255, 0, 0));
+		lblNewLabel_3_4.setBounds(300, 411, 239, 44);
+		contentPane.add(lblNewLabel_3_4);
+		
+		JLabel lblKetQua = new JLabel("");
+		lblKetQua.setEnabled(false);
+		lblKetQua.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblKetQua.setBounds(10, 264, 550, 74);
+		contentPane.add(lblKetQua);
 	}
 }
