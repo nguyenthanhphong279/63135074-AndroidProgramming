5961edab616fa2d2da82d4e600c9a743169dd79d
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-24 19:37:40 +0700

    Cập nhật giao diện

--- Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
+++ Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
@@ -6,38 +6,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class AppBMI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AppBMI frame = new AppBMI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public AppBMI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 584, 502);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Phần Mềm Tính Chỉ Số BMI");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		lblNewLabel.setBounds(118, 10, 339, 44);
+		contentPane.add(lblNewLabel);
 	}
-
 }
