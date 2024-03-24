8d464dcc7c5b3c2e0258ee802cc5af95640f1c61
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-24 19:45:26 +0700

    Cập nhật thêm giao diện AppBMI

--- Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
+++ Cau1_AppBMI/src/ntu/phongnt63135074/AppBMI.java
@@ -8,11 +8,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class AppBMI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	
 	public AppBMI() {
@@ -28,5 +32,29 @@ public class AppBMI extends JFrame {
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
 		lblNewLabel.setBounds(118, 10, 339, 44);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Chiều cao (cm):");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(10, 77, 177, 34);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Cân Nặng (kg):");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(10, 134, 165, 44);
+		contentPane.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(161, 80, 312, 34);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(161, 142, 312, 34);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Tính");
+		btnNewButton.setBounds(263, 216, 85, 21);
+		contentPane.add(btnNewButton);
 	}
 }
