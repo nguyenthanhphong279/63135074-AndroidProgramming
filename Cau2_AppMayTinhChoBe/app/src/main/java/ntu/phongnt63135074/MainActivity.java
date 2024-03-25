914db7a6e5272a84a2bf105ba65c5e1837240eb8
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-25 10:48:38 +0700

    Cập nhật code

--- Cau2_AppMayTinhChoBe/app/src/main/java/ntu/phongnt63135074/MainActivity.java
+++ Cau2_AppMayTinhChoBe/app/src/main/java/ntu/phongnt63135074/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.phongnt63135074;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    ImageButton nutCong, nutTru, nutNhan, nutChia;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TimDieuKhien();
+    }
+    void TimDieuKhien(){
+        editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        editTextKQ = (EditText)findViewById(R.id.edtKq);
+        nutCong = (ImageButton)findViewById(R.id.imgbtnCong);
+        nutTru = (ImageButton)findViewById(R.id.imgbtnTru);
+        nutNhan = (ImageButton)findViewById(R.id.imgbtnNhan);
+        nutChia = (ImageButton)findViewById(R.id.imgbtnChia);
+
     }
 }
