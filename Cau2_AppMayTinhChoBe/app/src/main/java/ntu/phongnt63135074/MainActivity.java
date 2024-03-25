71f42bec97a1d6b268a112609400c8dfb6ae932d
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-03-25 13:13:17 +0700

    Tạo hàm Random

--- Cau2_AppMayTinhChoBe/app/src/main/java/ntu/phongnt63135074/MainActivity.java
+++ Cau2_AppMayTinhChoBe/app/src/main/java/ntu/phongnt63135074/MainActivity.java
@@ -12,10 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     EditText editTextSo1;
     EditText editTextSo2;
     EditText editTextKQ;
+    Button btnRandom;
     ImageButton nutCong, nutTru, nutNhan, nutChia;
 
 
@@ -32,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
         TimDieuKhien();
         nutCong.setOnClickListener(boLangNghe_XuLyCong);
         nutTru.setOnClickListener(boLangNghe_XuLyTru);
-        nutNhan.setOnClickListener(boLangNghe_XuLyChia);
+        nutNhan.setOnClickListener(boLangNghe_XuLyNhan);
         nutChia.setOnClickListener(boLangNghe_XuLyChia);
+        btnRandom.setOnClickListener(boLangNghe_XuLyRandom);
 
 
     }
@@ -41,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         editTextSo1 = (EditText)findViewById(R.id.edtSo1);
         editTextSo2 = (EditText)findViewById(R.id.edtSo2);
         editTextKQ = (EditText)findViewById(R.id.edtKq);
+        btnRandom = (Button)findViewById(R.id.btnRandom);
         nutCong = (ImageButton)findViewById(R.id.imgbtnCong);
         nutTru = (ImageButton)findViewById(R.id.imgbtnTru);
         nutNhan = (ImageButton)findViewById(R.id.imgbtnNhan);
@@ -97,6 +102,19 @@ public class MainActivity extends AppCompatActivity {
             double Thuong = so1 / so2;
             String strKQ = String.valueOf(Thuong);
             editTextKQ.setText(strKQ);
+        }
+    };
+
+    View.OnClickListener boLangNghe_XuLyRandom = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int so1;
+            int so2;
+            Random rd = new Random();
+            so1 = rd.nextInt(10) + 1;
+            so2 = rd.nextInt(10) + 1;
+            editTextSo1.setText(String.valueOf(so1));
+            editTextSo2.setText(String.valueOf(so2));
         }
     };
 
