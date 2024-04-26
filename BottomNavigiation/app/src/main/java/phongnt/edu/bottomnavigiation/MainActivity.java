2da0b5b62e3e57bee57c2fffb3153f3096fe2aeb
Thanh Phong <phong.nt.63cntt@ntu.edu.vn>
2024-04-26 16:00:23 +0700

    Hoàn thành bài tập BottomNavigation

--- BottomNavigiation/app/src/main/java/phongnt/edu/bottomnavigiation/MainActivity.java
+++ BottomNavigiation/app/src/main/java/phongnt/edu/bottomnavigiation/MainActivity.java
@@ -1,12 +1,22 @@
 package phongnt.edu.bottomnavigiation;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +30,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        BottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.navHome){
+                    loadFragment(new HomeFragment(), false);
+                }
+                else if(itemId == R.id.navNotification){
+                    loadFragment(new NotificationFragment(), false);
+                }
+                else if(itemId == R.id.navProfile){
+                    loadFragment(new ProfileFragment(), false);
+                }
+                else{
+                    loadFragment(new SettingFragment(), false);
+                }
+                return true;
+            }
+        });
+
+        loadFragment(new HomeFragment(), true);
+
+    }
+
+    private void loadFragment(Fragment fragment, boolean checkAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(checkAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        }
+        else{
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ BottomNavigiation/app/src/main/java/phongnt/edu/bottomnavigiation/SettingFragment.java
@@ -0,0 +1,17 @@
+package phongnt.edu.bottomnavigiation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class SettingFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.setting_fragment, container, false);
+    }
+}
